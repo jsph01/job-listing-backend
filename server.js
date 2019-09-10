f0ea23c08f5ab20568a290ee5aa9d51225f68f56
jlister <jsphentrep14@gmail.com>
2019-09-10 11:29:40 -0700

    adjusted for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
+const path = require('path');
 const app = express();
 
 require('dotenv').config();
@@ -14,6 +15,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/posts', require('./routes/posts'));
 app.use('/api/replies', require('./routes/replies'));
+
+if(process.env.NODE_ENV === 'production' ) {
+    app.use(express.static('front-end/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
